025cc6ca31d0ca614cc1f97acc56d6aa67f5c6e7
PendingMitch <70475488+PendingMitch@users.noreply.github.com>
2022-01-30 17:22:46 +0000

    Lowered peaceful scenes to display priority status

--- datamanage.js
+++ datamanage.js
@@ -27,7 +27,7 @@ function resetData() {
 
 let upgrades_info = [
     {name: "Unnecessary 10-80", price: [{"type":"scenes", price: 10}], description: "Every time you click, you'll get another scene.", "function": "unnecessary1080", displayAt: 0},
-    {name: "Priority Status", price: [{"type":"scenes", price: 200}, {"type": "Peaceful Scenes", price: 25}], description: "Every time you click, you'll get another Peaceful Scenes.", "function": "priorityStatus", displayAt: [{"type":"scenes", price: 50}, {"type": "Peaceful Scenes", price: 5}]},
+    {name: "Priority Status", price: [{"type":"scenes", price: 200}, {"type": "Peaceful Scenes", price: 25}], description: "Every time you click, you'll get another Peaceful Scenes.", "function": "priorityStatus", displayAt: [{"type":"scenes", price: 50}, {"type": "Peaceful Scenes", price: 1}]},
     {name: "Unnecessary Shootout", price: [{"type":"scenes", price: 1000}, {"type": "Peaceful Scenes", price: 50}], description: "Every time you click, you'll get twice as many scenes.", "function": "unnecessaryShootout", displayAt: [{"type":"scenes", price: 200}, {"type": "Peaceful Scenes", price: 50}]},
     {name: "Priority Clear", price: [{"type":"scenes", price: 750}, {"type": "Peaceful Scenes", price: 250}], description: "Get another scene every 10 seconds.", "function": "autoclickerBought", displayAt: [{"type":"scenes", price: 500}, {"type": "Peaceful Scenes", price: 100}]},
     {name: "Josh is in Server", price: [{type:"scenes", price: 5000}, {type: "Peaceful Scenes", price: 2000}], description: "Every 10 seconds get another scene and a Peaceful Scenes.", "function": "autoclickerBought", displayAt: [{type: "scenes", amount: 750}, {type: "Peaceful Scenes", price: 250}]}
